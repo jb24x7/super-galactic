cde3c5969251c48a340968b7a38633cb94b73211
James Provance <jb24x7@gmail.com>
2023-02-06 03:51:12 -0800

    added onload function

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -5,15 +5,12 @@ describe('checkDate', () => {
 
   test('should correctly return a date with month day year', () => {
     const date = new Date();
-    
     expect(date.checkDate(1,2,2023)).toEqual('1, 2, 2023');
 
   });
 
   test('should correctly create a date with month day year depending on planet', () => {
     const date = new Date();
-    
-    
     expect(date.checkDate(1,2,2023,'earth')).toEqual('1, 2, 2023');
 
   });
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-////WIP
+window.onload () {
+  let month = document.getElementById("text1")
+  let day = document.getElementById("text2")
+  let year = document.getElementById("text3")
+  let planet = document.getElementById("planet")
+
+}
