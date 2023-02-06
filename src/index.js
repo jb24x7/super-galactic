16809feb8026fb9cd012a328e230635d5b51abf3
James Provance <jb24x7@gmail.com>
2023-02-06 04:04:40 -0800

    calls function in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 window.onload () {
-  let month = document.getElementById("text1")
-  let day = document.getElementById("text2")
-  let year = document.getElementById("text3")
-  let planet = document.getElementById("planet")
-  let text = document.getElementById("text")
+  let month = document.getElementById("text1");
+  let day = document.getElementById("text2");
+  let year = document.getElementById("text3");
+  let planet = document.getElementById("planet");
+  let text = document.getElementById("text");
+  let button = document.getElementById("button");
 
-  
+  button.onclick => {
+
+    checkDate(month, day, year, planet, text);
+
+  }
+ 
 
 }
