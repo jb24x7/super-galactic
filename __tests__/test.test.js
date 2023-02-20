9244cb853adf1137fd4f1db8c3f8ad9739e826a7
James Provance <jb24x7@gmail.com>
2023-02-19 18:08:57 -0800

    method now calls properly

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -1,34 +1,34 @@
-import Date from './../src/date';
+import Dateclass from './../src/date';
 
 
 describe('checkDate', () => {
 
   test('should correctly return a string saying your age in earth years', () => {
-    const date = new Date(2023);
+    const date = new Dateclass(2023);
     expect(date.checkDate('earth')).toEqual('you are 0 years old in earth years!');
 
   });
 
   test('should correctly return a string saying your age in mercury years', () => {
-    const date = new Date(2023);
+    const date = new Dateclass(2023);
     expect(date.checkDate('mercury')).toEqual('you are 0 years old in mercury years!');
 
   });
 
   test('should correctly return a string saying your age in venus years', () => {
-    const date = new Date(2023);
+    const date = new Dateclass(2023);
     expect(date.checkDate('venus')).toEqual('you are 0 years old in venus years!');
 
   });
 
   test('should correctly return a string saying your age in mars years', () => {
-    const date = new Date(2023);
+    const date = new Dateclass(2023);
     expect(date.checkDate('mars')).toEqual('you are 0 years old in mars years!');
 
   });
 
   test('should correctly return a string saying your age in jupiter years', () => {
-    const date = new Date(2023);
+    const date = new Dateclass(2023);
     expect(date.checkDate('jupiter')).toEqual('you are 0 years old in jupiter years!');
 
   });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import Date from './../src/date';
+import Dateclass from './../src/date';
 
 
 window.addEventListener("load", function () {
 
   let year = document.getElementById("text3");
   let planet = document.getElementById("planet");
+  let text = this.document.getElementById("text")
 
   planet.addEventListener("change", function () {
-    let newObj = new Date(year.value);
+    
+    const Obj = new Dateclass(year.value);
     let planetString = planet[planet.selectedIndex].value;
-    checkdate(planetString, newObj);
+    Obj.checkDate(planetString);
 
   });
 
