3276cbd9b1ca57a77eab44c9e0d1647d2f996a92
James Provance <jb24x7@gmail.com>
2023-02-19 18:58:12 -0800

    add new tests

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -4,31 +4,31 @@ import Dateclass from './../src/date';
 describe('checkDate', () => {
 
   test('should correctly return a string saying your age in earth years', () => {
-    const date = new Dateclass(2023, 2021);
+    const date = new Dateclass(2023, 2021, 2025);
     expect(date.checkDate('earth')).toEqual('you are 0 years old in earth years!');
 
   });
 
   test('should correctly return a string saying your age in mercury years', () => {
-    const date = new Dateclass(2023, 2021);
+    const date = new Dateclass(2023, 2021, 2025);
     expect(date.checkDate('mercury')).toEqual('you are 0 years old in mercury years!');
 
   });
 
   test('should correctly return a string saying your age in venus years', () => {
-    const date = new Dateclass(2023, 2021);
+    const date = new Dateclass(2023, 2021, 2025);
     expect(date.checkDate('venus')).toEqual('you are 0 years old in venus years!');
 
   });
 
   test('should correctly return a string saying your age in mars years', () => {
-    const date = new Dateclass(2023, 2021);
+    const date = new Dateclass(2023, 2021, 2025);
     expect(date.checkDate('mars')).toEqual('you are 0 years old in mars years!');
 
   });
 
   test('should correctly return a string saying your age in jupiter years', () => {
-    const date = new Dateclass(2023, 2021);
+    const date = new Dateclass(2023, 2021, 2025);
     expect(date.checkDate('jupiter')).toEqual('you are 0 years old in jupiter years!');
 
   });
@@ -38,33 +38,67 @@ describe('checkDate', () => {
 describe('checkYearsPassed', () => {
 
   test('should correctly return a string saying how many years have passed on the planet since your designated birthday', () => {
-    const date = new Dateclass(2002, 2007);
+    const date = new Dateclass(2002, 2007, 2025);
     expect(date.checkYearsPassed('earth')).toEqual('there has been 16 years since you turned 5 years old in earth years!');
 
   });
 
   test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
-    const date = new Dateclass(2002, 2007);
+    const date = new Dateclass(2002, 2007, 2025);
     expect(date.checkYearsPassed('mercury')).toEqual('there has been 66.66666666666667 years since you turned 5 years old in mercury years!');
 
   });
 
   test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
-    const date = new Dateclass(2002, 2007);
+    const date = new Dateclass(2002, 2007, 2025);
     expect(date.checkYearsPassed('venus')).toEqual('there has been 25.806451612903228 years since you turned 5 years old in venus years!');
 
   });
 
   test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
-    const date = new Dateclass(2002, 2007);
+    const date = new Dateclass(2002, 2007, 2025);
     expect(date.checkYearsPassed('mars')).toEqual('there has been 8.51063829787234 years since you turned 5 years old in mars years!');
 
   });
 
   test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
-    const date = new Dateclass(2002, 2007);
+    const date = new Dateclass(2002, 2007, 2025);
     expect(date.checkYearsPassed('jupiter')).toEqual('there has been 1.349072512647555 years since you turned 5 years old in jupiter years!');
 
   });
+
+});
+
+describe('checkYearsFuture', () => {
+
+  test('should correctly return a string saying how many years have passed on the planet since your designated birthday', () => {
+    const date = new Dateclass(2002, 2007, 2025);
+    expect(date.checkYearsFuture('earth')).toEqual('there will be 2 years until you turn 23 years old in earth years!');
+
+  });
+
+  test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
+    const date = new Dateclass(2002, 2007, 2025);
+    expect(date.checkYearsFuture('mercury')).toEqual('there has been 66.66666666666667 years since you turned 5 years old in mercury years!');
+
+  });
+
+  // test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
+  //   const date = new Dateclass(2002, 2007, 2025);
+  //   expect(date.checkYearsFuture('venus')).toEqual('there has been 25.806451612903228 years since you turned 5 years old in venus years!');
+
+  // });
+
+  // test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
+  //   const date = new Dateclass(2002, 2007, 2025);
+  //   expect(date.checkYearsFuture('mars')).toEqual('there has been 8.51063829787234 years since you turned 5 years old in mars years!');
+
+  // });
+
+  // test('should correctly return a string saying how many years have passed on the planet since your last birthday', () => {
+  //   const date = new Dateclass(2002, 2007, 2025);
+  //   expect(date.checkYearsFuture('jupiter')).toEqual('there has been 1.349072512647555 years since you turned 5 years old in jupiter years!');
+
+  // });
 
 });
--- src/date.js
+++ src/date.js
@@ -1,9 +1,10 @@
 export default class Dateclass {
 
-  constructor(yyyy, oldyear) {
+  constructor(yyyy, oldyear, futureyear) {
 
     this.year = yyyy;
     this.oldyear = oldyear;
+    this.futureyear = futureyear;
   }
 
   checkDate(planet) {
@@ -25,6 +26,20 @@ export default class Dateclass {
   checkYearsPassed(planet) {
     if (planet === 'earth') {
       return "there has been " + ((2023 - this.year) - (this.oldyear - this.year)) + " years since you turned " + (this.oldyear - this.year) + " years old in earth years!";
+    } else if (planet === 'mercury') {
+      return "there has been " + (((2023 - this.year) - (this.oldyear - this.year)) / .24) + " years since you turned " + (this.oldyear - this.year) + " years old in mercury years!";
+    } else if (planet === 'venus') {
+      return "there has been " + (((2023 - this.year) - (this.oldyear - this.year)) / .62) + " years since you turned " + (this.oldyear - this.year) + " years old in venus years!";
+    } else if (planet === 'mars') {
+      return "there has been " + (((2023 - this.year) - (this.oldyear - this.year)) / 1.88) + " years since you turned " + (this.oldyear - this.year) + " years old in mars years!";
+    } else if (planet === 'jupiter') {
+      return "there has been " + (((2023 - this.year) - (this.oldyear - this.year)) / 11.86) + " years since you turned " + (this.oldyear - this.year) + " years old in jupiter years!";
+    }
+  }
+
+  checkYearsFuture(planet) {
+    if (planet === 'earth') {
+      return "there will be " + (this.futureyear - 2023) + " years until you turn " + (this.futureyear - this.year) + " years old in earth years!";
     } else if (planet === 'mercury') {
       return "there has been " + (((2023 - this.year) - (this.oldyear - this.year)) / .24) + " years since you turned " + (this.oldyear - this.year) + " years old in mercury years!";
     } else if (planet === 'venus') {
