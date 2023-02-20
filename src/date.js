3753cf783eee3c62251d892d3107f3c71fe1ab6b
James Provance <jb24x7@gmail.com>
2023-02-19 16:16:42 -0800

    test now has full line coverage and passes

--- src/date.js
+++ src/date.js
@@ -4,19 +4,17 @@ export default class Date {
     this.year = yyyy;
   }
 
-  checkDate(planet, year) {
-    
-
+  checkDate(planet) {
     if (planet === 'earth') {
-    return "you are " + (2023 - year) + " years old in earth years!";
+      return "you are " + (2023 - this.year) + " years old in earth years!";
     } else if (planet === 'mercury') {
-      return "you are " + ((2023 - year) / .24) + " years old in mercury years!";
+      return "you are " + ((2023 - this.year) / .24) + " years old in mercury years!";
     } else if (planet === 'venus') {
-      return "you are " + ((2023 - year) / .62) + " years old in venus years!";
+      return "you are " + ((2023 - this.year) / .62) + " years old in venus years!";
     } else if (planet === 'mars') {
-      return "you are " + ((2023 - year) / 1.88) + " years old in mars years!";
+      return "you are " + ((2023 - this.year) / 1.88) + " years old in mars years!";
     } else if (planet === 'jupiter') {
-      return "you are " + ((2023 - year) / 11.86) + " years old in jupiter years!";
+      return "you are " + ((2023 - this.year) / 11.86) + " years old in jupiter years!";
     } else {
       return "Please select a planet!";
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 window.onload () {
-  let month = document.getElementById("text1");
-  let day = document.getElementById("text2");
   let year = document.getElementById("text3");
   let planet = document.getElementById("planet");
-  let text = document.getElementById("text");
   let button = document.getElementById("button");
 
   button.onclick => {
